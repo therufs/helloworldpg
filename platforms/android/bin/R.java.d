57d39366bf39b45e3d8c80f7c23b35e5f0bd58da
therufs <alphasqix@gmail.com>
2013-11-05 12:14:56 -0500

    About to start tweaking things

---
+++ platforms/android/bin/R.java.d
@@ -0,0 +1,9 @@
+/home/osi/hello/platforms/android/gen/com/example/hello/R.java \
+ : /home/osi/hello/platforms/android/res/drawable-mdpi/icon.png \
+/home/osi/hello/platforms/android/res/drawable/icon.png \
+/home/osi/hello/platforms/android/res/drawable-xhdpi/icon.png \
+/home/osi/hello/platforms/android/res/xml/config.xml \
+/home/osi/hello/platforms/android/res/drawable-hdpi/icon.png \
+/home/osi/hello/platforms/android/res/values/strings.xml \
+/home/osi/hello/platforms/android/res/drawable-ldpi/icon.png \
+/home/osi/hello/platforms/android/bin/AndroidManifest.xml \
